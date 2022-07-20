62c47b22f0a1c04f5cb679162f5f55ad5c4f33d3
barryos112 <barryos112@gmail.com>
2022-07-20 11:40:57 +0100

    Social links go to new tab

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+
 import {HiArrowNarrowRight} from 'react-icons/hi'
 
+
+
 export const Home = () => {
+
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
@@ -18,7 +22,7 @@ export const Home = () => {
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'> View Work 
                 <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-3'/>
-
+             
                 </span>
                 </button>
             </div>
@@ -29,3 +33,4 @@ export const Home = () => {
     </div>
   )
 }
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/barry-o-sullivan-33425b200/' target="_blank" rel="noopener noreferrer"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -109,7 +109,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/barryos112' target="_blank" rel="noopener noreferrer"
             >
               Github <FaGithub size={30} />
             </a>
@@ -125,9 +125,9 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://docs.google.com/document/d/1bQ3sKcNegQ_QJ_divzr88kMBer_6tk0aqhZXb34rORc/edit?usp=sharing' target="_blank" rel="noopener noreferrer"
             >
-              Resume <BsFillPersonLinesFill size={30} />
+              CV <BsFillPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
